8f6173b0d0bf0879e12bb2cf841a22a21817a0bb
khaspalaev <khaspalaevan05@gmail.com>
2025-04-11 22:25:35 +0300

    feat: добавлен splash screen

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+// import { splashScreen } from "./scripts/SplashScreen";
 import { animateNumbers } from "./scripts/AnimateNumbers";
 import { isElementInViewport } from "./scripts/utilis";
 
-const body = document.querySelector('.page')
+const body = document.querySelector('.page');
+const header = document.querySelector('.header');
+const splash = document.querySelector('.splash');
 const dropdown = document.querySelector('.header__dropdown');
 const headerNavigation = document.querySelector('.header__menu');
-const headerNavigationItems = document.querySelectorAll('.header__menu-item')
+const headerNavigationItems = document.querySelectorAll('.header__menu-item');
 
+// Splash Screen
+header.style.display = 'none';
+setTimeout(() => {
+    splash.classList.add('splash_animated');
+    header.style.display = 'block';
+}, 2000);
+
+
+// Работа дропдауна
 dropdown.addEventListener('click', () => {
     if (!headerNavigation.classList.contains('header__menu_active')) {
         headerNavigation.classList.add('header__menu_active');
@@ -16,8 +28,7 @@ dropdown.addEventListener('click', () => {
         dropdown.classList.remove('header__dropdown_active');
         body.style.position = 'static'
     }
-    
-})
+});
 
 headerNavigationItems.forEach((link) => {
     link.addEventListener('click', () => {
@@ -27,8 +38,10 @@ headerNavigationItems.forEach((link) => {
             body.style.position = 'static'
         }
     })
-})
+});
 
+
+// Анимация чисел в разделе услуг
 document.addEventListener('DOMContentLoaded', function() {
     const statNumbers = document.querySelectorAll('.stats__item-number');
     const statsSection = document.querySelector('.stats');
